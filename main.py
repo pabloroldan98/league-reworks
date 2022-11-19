4709919554e1b150abd30e66ab4f1426803eb64c
Pablo Roldan <pablo.roldan@devo.com>
2022-11-19 14:13:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from player import Player
+
+p1 = Player("John", 36)
+
+print(p1)
---
+++ player.py
@@ -0,0 +1,25 @@
+class Player:
+    def __init__(
+            self,
+            name: str,
+            position: str,
+            price: float,
+            value: float
+    ):
+        self.name = name
+        self.position = position
+        self.price = price
+        self.value = value
+
+    def __str__(self):
+        return f"({self.name}, {self.position}, {self.price}, {self.value})"
+
+    @property
+    def position(self):
+        return self.position
+
+    @position.setter
+    def position(self, pos):
+        if pos not in ["GK", "DEF", "MID", "ATT"]:
+            raise ValueError("Sorry, that's not a valid position")
+        self.position = pos
